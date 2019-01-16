b389e84c1517a6178b09a738bae35273b89a32bf
谢烨 <hatehate123@126.com>
2019-01-16 17:56:26 +0800

    添加测试队列的文件

---
+++ workspace/myapp_code1/beanstalkd_test.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once( __DIR__ . '/vendor/autoload.php' );
+use Pheanstalk\Pheanstalk;
+
+
+
+header("Content-type: text/html; charset=utf-8");
+
+echo "<h1>this is app1</h1>";		
+
+
+
+$beanstalkd =Sys::get_beanstalkd();
+
+//这是消息数据
+$delay=0;
+$data=['a'=>66,'time'=>time() ];
+$queue_name ="q1";
+// 把消息放入队列,1024 是优先级
+//$delay 非常重要，假设设置10，则该消息10秒后才被放入队列！非常好使。
+$beanstalkd->useTube( $queue_name  )
+            ->put(serialize($data), 1024, $delay);
+
+echo "<br>现在已经往队列插入一条数据，是 ". serialize($data);
+
+
+echo "<h1>下面是从队列取出一条数据，数据是：</h1>";		
+
+
+$job = $beanstalkd->watch( $queue_name )->ignore('default')->reserve();
+$job_data =  $job->getData() ;
+var_dump($job_data);
+
+echo "<br><br>如果看到数据，说明队列服务端和客户端全部安装成功";
+
+//删除千万不能忘记。
+$beanstalkd->delete($job);
+
+class Sys
+{
+	
+    /**
+     * 
+     * @return 
+     */
+    public static function get_beanstalkd()
+    {
+        $beanstalkd = new Pheanstalk('beanstalkd','11300');
+        return $beanstalkd;    
+    }
+	
+	
+	
+}
+
--- workspace/myapp_code1/index.php
+++ workspace/myapp_code1/index.php
@@ -1,3 +1,17 @@
 <?php
-echo "app1";
+echo "<h1>app1</h1>";
+
+echo "
+<p>
+ <a href='redis_db_test.php'>测试数据库和 redis 连接</a>
+</p>
+
+";
+echo "
+<p>
+ <a href='beanstalkd_test.php'>测试队列 beanstalkd 连接，请事先 composer install </a>
+</p>
+
+";
+
 phpinfo();
